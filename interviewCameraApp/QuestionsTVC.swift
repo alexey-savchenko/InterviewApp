0179f8ad4507e4e0b979f6f263cb684a57094b1e
Alexey Savchenko <DreDD@Alexeys-MacBook-Pro.local>
2017-09-19 10:33:33 +0300

    Init commmit

---
+++ interviewCameraApp/QuestionsTVC.swift
@@ -0,0 +1,130 @@
+//
+//  QuestionsTVC.swift
+//  interviewCameraApp
+//
+//  Created by Alexey Savchenko on 19.09.17.
+//  Copyright © 2017 svchdzgn. All rights reserved.
+//
+
+import UIKit
+
+class QuestionsTVC: UITableViewController {
+  
+  var questions = ["What is your favorite color?",
+                   "What icecream do you like?",
+                   "Where are you from?"]
+  
+  var editButton: UIBarButtonItem!
+  var addButton: UIBarButtonItem!
+  
+  
+  var isInEditingMode = false {
+    
+    didSet {
+      
+      if isInEditingMode {
+        
+        tableView.setEditing(false, animated: true)
+        editButton.title = "Edit"
+      } else {
+        
+        tableView.setEditing(true, animated: true)
+        editButton.title = "Done"
+        
+      }
+      
+    }
+    
+  }
+  
+  var addQuestionPrompt: UIAlertController {
+    
+    let alert = UIAlertController(title: "Add a question", message: "Enter a question below.", preferredStyle: .alert)
+    
+    alert.addAction(UIAlertAction.init(title: "Done", style: .default, handler: { (_) in
+      
+      let question = alert.textFields!.first!.text!
+      
+      if question != "" {
+        
+        self.questions.append(question)
+        self.tableView.reloadData()
+        
+      }
+      
+    }))
+    
+    alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+    
+    alert.addTextField(configurationHandler: nil)
+    
+    return alert
+    
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    editButton = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editTap))
+    navigationItem.leftBarButtonItems = [editButton]
+    
+    addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTap))
+    navigationItem.rightBarButtonItems = [addButton]
+    
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    
+    
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    
+    
+    
+  }
+  
+  
+  func addTap() {
+    
+    present(addQuestionPrompt, animated: true, completion: nil)
+    
+  }
+  
+  func editTap(){
+    
+    isInEditingMode = !isInEditingMode
+    
+  }
+  
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    return questions.count
+  }
+  
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    
+    cell.textLabel?.text = questions[indexPath.row]
+    cell.selectionStyle = .none
+    return cell
+  }
+  
+  
+  
+  // Override to support editing the table view.
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      // Delete the row from the data source
+      questions.remove(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
+    }
+  }
+  
+}
---
+++ interviewCameraApp/ViewController.swift
@@ -0,0 +1,152 @@
+//
+//  ViewController.swift
+//  interviewCameraApp
+//
+//  Created by Alexey Savchenko on 18.09.17.
+//  Copyright © 2017 svchdzgn. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AVKit
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureFileOutputRecordingDelegate {
+
+  let captureSession = AVCaptureSession()
+  var previewLayer: CALayer!
+  var captureDevice: AVCaptureDevice!
+  var movieOutput: AVCaptureMovieFileOutput!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    prepareCamera()
+  }
+
+  
+  func prepareCamera() {
+  
+    captureSession.sessionPreset = AVCaptureSessionPresetHigh
+    
+    if let availableDevices = AVCaptureDeviceDiscoverySession(deviceTypes: [.builtInWideAngleCamera],
+                                                              mediaType: AVMediaTypeVideo,
+                                                              position: .front).devices{
+      captureDevice = availableDevices.first!
+      beginSession()
+    }
+    
+  }
+  
+  func beginSession() {
+  
+    do {
+      let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+      captureSession.addInput(captureDeviceInput)
+//      AVCaptureDevice *audioDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeAudio];
+//      AVCaptureDeviceInput * audioInput = [AVCaptureDeviceInput deviceInputWithDevice:audioDevice error:nil];
+//      [session addInput:audioInput]
+      let audiodevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeAudio)
+      let audioinput = try AVCaptureDeviceInput(device: audiodevice)
+      captureSession.addInput(audioinput)
+    } catch {
+      print(error.localizedDescription)
+    }
+    
+    if let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) {
+    
+      self.previewLayer = previewLayer
+      let preview = UIView()
+      preview.frame = view.frame
+      view.addSubview(preview)
+      view.sendSubview(toBack: preview)
+      preview.layer.addSublayer(self.previewLayer)
+      
+//      view.layer.addSublayer(self.previewLayer)
+      self.previewLayer.frame = preview.layer.frame
+      captureSession.startRunning()
+      
+//      let dataOutput = AVCaptureVideoDataOutput()
+//      dataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as String): NSNumber.init(value: kCVPixelFormatType_32BGRA)]
+//      dataOutput.alwaysDiscardsLateVideoFrames = true
+//      
+//      
+//      
+//      
+//      if captureSession.canAddOutput(dataOutput) {
+//      
+//        captureSession.addOutput(dataOutput)
+//        
+//      }
+//      
+//      captureSession.commitConfiguration()
+//      
+//      let queue = DispatchQueue(label: "com.svch.queue")
+//      
+//      dataOutput.setSampleBufferDelegate(self, queue: queue)
+      movieOutput = AVCaptureMovieFileOutput()
+      if captureSession.canAddOutput(movieOutput) {
+        captureSession.addOutput(movieOutput)
+      }
+      
+      captureSession.commitConfiguration()
+      
+      movieOutput.startRecording(toOutputFileURL: getDocumentsDirectory().appendingPathComponent("video.mov"),
+                                 recordingDelegate: self)
+      
+    }
+    
+  }
+  @IBAction func buttontap(_ sender: UIButton) {
+    movieOutput.stopRecording()
+    do {
+      
+      print("Contents of Doc folder \(try FileManager.default.contentsOfDirectory(atPath: NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!))")
+      if FileManager.default.fileExists(atPath: getDocumentsDirectory().appendingPathComponent("video.mov").path) {
+        
+        let size = try! FileManager.default.attributesOfItem(atPath: getDocumentsDirectory().appendingPathComponent("video.mov").path)[FileAttributeKey.size] as! UInt64
+        print(size)
+        
+        
+      } else {
+        
+        print("no file")
+        
+      }
+    } catch {
+      
+      print(error)
+      
+    }
+  }
+  
+  @IBAction func nextBtnTap(_ sender: UIBarButtonItem) {
+    
+    
+    
+  }
+  
+  func capture(_ captureOutput: AVCaptureFileOutput!,
+               didStartRecordingToOutputFileAt fileURL: URL!,
+               fromConnections connections: [Any]!) {
+    print("22")
+  }
+  func capture(_ captureOutput: AVCaptureFileOutput!,
+               didFinishRecordingToOutputFileAt outputFileURL: URL!,
+               fromConnections connections: [Any]!, error: Error!) {
+    print("23")
+    let player = AVPlayer(playerItem: AVPlayerItem.init(url: URL(fileURLWithPath: getDocumentsDirectory().appendingPathComponent("video.mov").path)))
+    let plContr = AVPlayerViewController()
+    plContr.player = player
+    self.present(plContr, animated: true) { 
+      print("presented")
+      plContr.player?.play()
+    }
+    
+  }
+  
+  
+  func getDocumentsDirectory() -> URL{
+    return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+  }
+
+}
+
